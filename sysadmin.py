37affa297bcf2974018dc4061326466cef89bcab
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2025-10-25 06:08:11 -0400

    Update sysadmin.py

--- sysadmin.py
+++ sysadmin.py
@@ -37,7 +37,19 @@ def main():
                 water_str = parts[1].split(':')[1]
                 temperature = float(temp_str)
                 water_level = int(water_str)                
-                print(f"Temperature: {temperature}°C, Water Level: {water_level}")
+                # print(f"Temperature: {temperature}°C, Water Level: {water_level}")
+                def state():
+					pass
+                i2c = I2C(0, sda=Pin(21, pull=Pin.PULL_UP), scl=Pin(22, pull=Pin.PULL_UP))
+                screen = SSD1306_I2C(128, 64, i2c)
+                screen.fill(0)
+                screen.rect(0, 0, 127, 63, 1)
+                screen.text("WATERP: ", 5, 2, 1)
+                screen.text("AIRP: ", 5, 12, 1)
+                screen.text("WATERL: " + water_level, 5, 22, 1)
+                screen.text("AIRT: " + temperature, 5, 32, 1)
+                screen.text("STATE: " + state, 5, 42, 1)
+                screen.show()
                 
                 if water_level < WATER_THRESHOLD:
                     print("Water sensor triggered. Turning Water Pump ON.")
